07e7d58c5e52d928cdc8de7e4cd30e60826ba766
Piyushzingade <piyushzingade@gmail.com>
2025-05-02 09:42:26 +0530

    transaction and p2p is working

--- apps/user-app/components/SendCard.tsx
+++ apps/user-app/components/SendCard.tsx
@@ -4,6 +4,7 @@ import { Card } from "@repo/ui/card";
 import { Center } from "@repo/ui/Center";
 import { TextInput } from "@repo/ui/TextInput";
 import { useState } from "react";
+import { p2pTransfer } from "../app/lib/actions/p2pTransaction";
 // import {p2pTransfer} from "../app/lib/actions/p2pTransaction";
 
 
@@ -26,7 +27,7 @@ export function SendCard() {
                         <Button 
                             className="border border-slate-500 text-slate-700 bg-transparent p-2 px-10 rounded-xl hover:text-white  hover:bg-[#6a51a6]"
                             onClick={async () => {
-                            // await p2pTransfer(number, Number(amount) * 100)
+                            await p2pTransfer(number, Number(amount) * 100)
                         }}>Send</Button>
                     </div>
                 </div>
